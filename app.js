9ec4891c5a75c122853908ccb9a3db58bb63d75b
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-03-31 08:56:05 -0700

    app works

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const resetButton = document.getElementById('reset-button');
 const guess = document.getElementById('guess');
 const results = document.getElementById('results');
 
-let MagicNumber = 
-const  = document.getElementById('button');
-const  = document.getElementById('button');
+let magicNumber = Math.floor(Math.random() * 20);
+
+button.addEventListener('click', () => {
+    
+    let userGuess = Number(guess.value);
+    const result= guessNumber(userGuess, magicNumber);
+
+    if (result === 0) {
+        results.textContent =`we have a draw!`;
+    }
+    else if (result === 1) {
+        results.textContent = `too high`;
+    }
+        else if(result === -1) {
+        results.textContent = `too low`;
+    }
+}
+)
+
+
+
